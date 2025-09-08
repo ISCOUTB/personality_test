0e89bd21fa799afc842e2d2abb4a03baf131caec
Server Dev <server@saviodev.utb.edu.co>
2025-09-08 23:16:59 +0000

    Fix GitHub Actions workflow version extraction

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025090803;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2025090804;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100;        // Requires this Moodle version (4.5)
 $plugin->component = 'block_personality_test';  // Full name of the plugin
 $plugin->maturity  = MATURITY_STABLE;
