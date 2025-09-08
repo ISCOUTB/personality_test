fb4309ff8779c6d99bbc84db03fe158732026acf
Server Dev <server@saviodev.utb.edu.co>
2025-09-08 23:00:38 +0000

    actualizada la versión

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2022011108;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2025090801;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100;        // Requires this Moodle version (4.5)
 $plugin->component = 'block_personality_test';  // Full name of the plugin
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.0';           // Human-readable version name
+$plugin->release   = '1.1';           // Human-readable version name
