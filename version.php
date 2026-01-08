07eb08704f37d8211f29ef485e4383a9f207a570
Isaac David Sánchez Sánchez <158796340+IsaacDev2024@users.noreply.github.com>
2026-01-08 00:19:13 -0500

    Update version to 2.0.2

--- version.php
+++ version.php
@@ -1,17 +1,17 @@
 <?php
 /**
  * Personality Test Block Major Update
- * Version 2.0.1 - Production Ready
+ * Version 2.0.2 - Production Ready
  *
  * @package    block_personality_test
- * @copyright  2025 Jairo Serrano, Yuranis Henriquez, Isaac Sanchez, Santiago Orejuela, Maria Valentina
+ * @copyright  2026 Jairo Serrano, Yuranis Henriquez, Isaac Sanchez, Santiago Orejuela, Maria Valentina
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025122200;
-$plugin->requires = 2022112800;
+$plugin->version = 2026010800;
+$plugin->requires = 2022041900; // Moodle 4.0+
 $plugin->component = 'block_personality_test';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.1';
+$plugin->release = '2.0.2';
